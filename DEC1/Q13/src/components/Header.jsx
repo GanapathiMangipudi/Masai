4e2a5ef3d6c0839868cb2dde23a799987fe3d3d1
GanapathiMangipudi <57028826+GanapathiMangipudi@users.noreply.github.com>
2025-12-01 23:40:19 +0530

    Create Header.jsx

---
+++ DEC1/Q13/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  const styles = {
+    background: "#2c3e50",
+    padding: "15px",
+    color: "white",
+    fontSize: "20px",
+    textAlign: "center",
+  };
+
+  return <header style={styles}>My Header</header>;
+}
+
+export default Header;
