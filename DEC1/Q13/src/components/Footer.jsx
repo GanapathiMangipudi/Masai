72bfd5c17a16d9060cf7567d11d0aa99659d41e3
GanapathiMangipudi <57028826+GanapathiMangipudi@users.noreply.github.com>
2025-12-01 23:41:53 +0530

    Create Footer.jsx

---
+++ DEC1/Q13/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer() {
+  const style = {
+    background: "#ddd",
+    padding: "12px",
+    textAlign: "center",
+    fontSize: "14px"
+  };
+
+  return <footer style={style}>2025 My Footer</footer>;
+}
+
+export default Footer;
